d6424d2cab8cd02b80517c0fd2ebe163f7066409
Poojesh Shetty <poojesh@wednesday.is>
2023-02-07 11:52:37 +0530

    chore: lint fix

--- resolver/auth_mutations.resolvers.go
+++ resolver/auth_mutations.resolvers.go
@@ -60,7 +60,11 @@ func (r *mutationResolver) Login(ctx context.Context, username string, password 
 }
 
 // ChangePassword is the resolver for the changePassword field.
-func (r *mutationResolver) ChangePassword(ctx context.Context, oldPassword string, newPassword string) (*gqlmodels.ChangePasswordResponse, error) {
+func (r *mutationResolver) ChangePassword(
+	ctx context.Context,
+	oldPassword string,
+	newPassword string,
+) (*gqlmodels.ChangePasswordResponse, error) {
 	userID := auth.UserIDFromContext(ctx)
 	u, err := daos.FindUserByID(userID, ctx)
 	if err != nil {
@@ -125,9 +129,9 @@ type mutationResolver struct{ *Resolver }
 // !!! WARNING !!!
 // The code below was going to be deleted when updating resolvers. It has been copied here so you have
 // one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+//   - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//     it when you're done.
+//   - You have helper methods in this file. Move them out to keep these resolver files clean.
 func loadConfig() (*config.Configuration, error) {
 	cfg, err := config.Load()
 	if err != nil {
